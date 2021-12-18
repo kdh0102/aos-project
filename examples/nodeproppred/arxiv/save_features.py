d9835d79994add8ef58d2011160c6ae8ad6817ab
tntjd7545 <tntjd7545@gmail.com>
2021-12-18 17:03:03 +0900

    save new_index

--- examples/nodeproppred/arxiv/save_features.py
+++ examples/nodeproppred/arxiv/save_features.py
@@ -19,9 +19,10 @@ def save_raw(path, data, num_nodes, split_idx=None):
     out.close()
 
 def save_new_graph(data, new_index_table, new_index_sorted, num_nodes):
-
+    
     new_adj_t = []
     new_embedding = []
+    new_index = []
     embedding = data.x.numpy()
 
     for index in new_index_sorted:
@@ -35,6 +36,12 @@ def save_new_graph(data, new_index_table, new_index_sorted, num_nodes):
         new_adj_t.append(new_neighbors)
 
     # Store reorganized embedding, adj matrix
+    out = open("new_index.txt", "w")
+    for i in range(num_nodes):
+        new_index[i] = list(map(str, new_index_table[i][1]))
+        print(" ".join(new_index[i]), file=out)
+    out.close()
+
     out = open("new_embedding.txt", "w")
     for i in range(num_nodes):
         new_embedding[i] = list(map(str, new_embedding[i]))
