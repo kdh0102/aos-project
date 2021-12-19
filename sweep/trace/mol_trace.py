d79c4fb5a34f1611d4d3a6ed3396bf1bb65fe843
hjbc0921 <hjbc0921@gmail.com>
2021-12-19 14:04:24 +0900

    [simulator] arxiv sweep simulator

---
+++ sweep/trace/mol_trace.py
@@ -0,0 +1,81 @@
+from  dataclasses import dataclass, field
+from typing import List
+from pathlib import Path
+
+from trace.trace import Trace
+from utils import sanity_check, open_data_trace_files, get_sentence_len, timer
+
+@dataclass
+class MolIOEvent():
+    graph_idx: int
+    node_start: int
+    node_count: int
+    edge_start: int
+    edge_count: int
+    y_value: int
+
+    @classmethod
+    def from_trace(cls, items: List[str]):
+        return cls(*[int(item) for item in items])
+
+@dataclass
+class MolTrace(Trace):
+    events: List[MolIOEvent] = field(default_factory=list)
+
+    def simulate(self, data_file_path: Path):
+        """Run simulation on the storage for mol dataset.
+
+        Args:
+            data_file_path(Path): The data file path. Make sure the file is located in the storage
+                                  that you want to simulate.
+
+        """
+        sanity_check(data_file_path)
+        file_handlers = open_data_trace_files(data_file_path)
+        sentence_len = get_sentence_len(file_handlers)
+
+        self.handler_x, self.len_x = file_handlers["x"], sentence_len["x"]
+        self.handler_edge_attr, self.len_edge_attr = file_handlers["edge_attr"], sentence_len["edge_attr"]
+
+        event_durations = []
+        for event in self.events:
+            event_durations.append(self._iterate_event(event))
+
+        return event_durations
+
+    @timer
+    def _iterate_event(self, event):
+        # Read node.x file
+        self.handler_x.seek(event.node_start * self.len_x)
+        self.data_x = self.handler_x.read(event.node_count * self.len_x)
+
+        self.handler_edge_attr.seek(event.edge_start * self.len_edge_attr)
+        self.data_edge_attr = self.handler_edge_attr.read(event.edge_count * self.len_edge_attr)
+
+
+
+    @classmethod
+    def parse_trace_file(cls, trace_file: str):
+        """Parse the trace file.
+
+        The trace file, for example, can be the access sequences of a GNN model.
+
+        Args:
+            trace_file(str): The trace file path.
+            
+        """
+        events = []
+        try:
+            with open(trace_file, "r") as f:
+                while True:
+                    line = f.readline()
+                    if not line:
+                        break
+                    # Assumption: The first line contains `#` in the beginning.
+                    if "#" in line:
+                        continue
+                    events.append(MolIOEvent.from_trace(line.split("\t")))
+        except:
+            raise FileExistsError(f"Trace file({trace_file}) does not exist.")
+
+        return cls(events)
---
+++ sweep/utils.py
@@ -0,0 +1,65 @@
+import time
+from pathlib import Path
+from dataclasses import dataclass
+from multiprocessing import Process
+from subprocess import call
+
+from constants import SPLIT_FILES, DATA_TRACE_FILES, STORAGE
+
+
+def timer(func):
+    # a decorator to record the duration
+    # Note that this decorator will return the function duration, instead of the function output.
+    def _timer(*args, **kwargs):
+        start = time.time()
+        func(*args, **kwargs)
+        end = time.time()
+        return (end - start) * 1e9
+    return _timer
+
+
+@dataclass
+class Blktrace:
+    BLKTRACE = "blktrace"
+
+    @staticmethod
+    def run() -> Process:
+        def call_blktrace():
+            call(["sudo", Blktrace.BLKTRACE, "-d", STORAGE])
+
+        p = Process(target=call_blktrace, daemon=True)
+        return p
+
+
+def record_blktrace(func):
+    def blktrace(*args, **kwargs):
+        p = Blktrace.run()
+        p.start()
+        result = func(*args, **kwargs)
+        p.kill()
+        p.join()
+        return result
+    return blktrace
+
+
+def sanity_check(data_file_path: Path):
+    for file in {**SPLIT_FILES, **DATA_TRACE_FILES}.values():
+        assert((data_file_path / file).exists())
+
+
+def open_data_trace_files(data_file_path):
+    handlers = {}
+    # for key, fname in DATA_TRACE_FILES.items():
+        # if (data_file_path / fname).exists():
+    handlers['x'] = open(data_file_path / "data.x.txt", "r")
+
+    return handlers
+
+
+def get_sentence_len(handlers):
+    sentence_len = {}
+    for key, file_handler in handlers.items():
+        sentence_len[key] = len(file_handler.readline())
+        file_handler.seek(0)
+
+    return sentence_len
